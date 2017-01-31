17ac18564aad0ef1e293e01dc40f0f534043a88b
Keuvain <k94ll13nn3@outlook.fr>
2017-01-31 21:59:59 +0100

    Add support for base filter (un)registration (#26)

--- src/Strinken/Filters/FilterHelpers.cs
+++ src/Strinken/Filters/FilterHelpers.cs
@@ -1,5 +1,6 @@
 ﻿// stylecop.header
 using System.Collections.Generic;
+using System.Linq;
 using Strinken.Parser;
 
 namespace Strinken.Filters
@@ -7,14 +8,24 @@ namespace Strinken.Filters
     /// <summary>
     /// Base class to share data amongst all parsers.
     /// </summary>
-    internal static class FilterHelpers
+    public static class FilterHelpers
     {
+        /// <summary>
+        /// Lock object for operations on the filters list.
+        /// </summary>
+        private static readonly object Lock = new object();
+
+        /// <summary>
+        /// The list of registered filters.
+        /// </summary>
+        private static readonly IDictionary<string, IFilter> RegisteredFilters;
+
         /// <summary>
         /// Initializes static members of the <see cref="FilterHelpers"/> class.
         /// </summary>
         static FilterHelpers()
         {
-            BaseFilters = new List<IFilter>
+            var baseFilters = new List<IFilter>
             {
                 new UpperFilter(),
                 new LengthFilter(),
@@ -24,11 +35,56 @@ namespace Strinken.Filters
                 new IfEqualFilter(),
                 new ReplaceFilter()
             };
+
+            RegisteredFilters = baseFilters.ToDictionary(x => x.Name, x => x);
         }
 
         /// <summary>
         /// Gets base filters shared by all parsers.
         /// </summary>
-        internal static IEnumerable<IFilter> BaseFilters { get; }
+        internal static IEnumerable<IFilter> BaseFilters
+        {
+            get
+            {
+                lock (Lock)
+                {
+                    return RegisteredFilters.Values;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Registers a filter that will be used as a base filter for all parser built after.
+        /// </summary>
+        /// <typeparam name="T">The type of the filter.</typeparam>
+        /// <param name="filter">The filter to register.</param>
+        public static void Register<T>(T filter)
+            where T : IFilter
+        {
+            lock (Lock)
+            {
+                if (!RegisteredFilters.ContainsKey(filter.Name))
+                {
+                    RegisteredFilters.Add(filter.Name, filter);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Unregisters a filter from the base filters.
+        /// </summary>
+        /// <typeparam name="T">The type of the filter.</typeparam>
+        /// <param name="filter">The filter to unregister.</param>
+        public static void UnRegister<T>(T filter)
+            where T : IFilter
+        {
+            lock (Lock)
+            {
+                if (RegisteredFilters.ContainsKey(filter.Name))
+                {
+                    RegisteredFilters.Remove(filter.Name);
+                }
+            }
+        }
     }
 }
---
+++ test/Strinken.Public.Tests/Filters/FilterHelpersTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Strinken.Filters;
+using Strinken.Parser;
+using Strinken.Public.Tests.TestsClasses;
+
+namespace Strinken.Public.Tests.Filters
+{
+    [TestFixture]
+    public class FilterHelpersTests
+    {
+        [Test]
+        public void Validate_FilterRegistered_ReturnsTrue()
+        {
+            FilterHelpers.Register(new SomeFilter());
+            Parser<Data> stringSolver = new Parser<Data>().WithTag(new DataNameTag());
+            var validationResult = stringSolver.Validate("The {DataName:Some} is in the kitchen.");
+            Assert.That(validationResult.IsValid, Is.True);
+
+            FilterHelpers.UnRegister(new SomeFilter());
+        }
+
+        [Test]
+        public void Validate_FilterRegisteredAfterValidation_ReturnsFalse()
+        {
+            Parser<Data> stringSolver = new Parser<Data>().WithTag(new DataNameTag());
+            FilterHelpers.Register(new SomeFilter());
+            var validationResult = stringSolver.Validate("The {DataName:Some} is in the kitchen.");
+            Assert.That(validationResult.IsValid, Is.False);
+
+            FilterHelpers.UnRegister(new SomeFilter());
+        }
+
+        [Test]
+        public void Validate_FilterRegisteredAndThenUnRegistered_ReturnsTrueAndThenFalse()
+        {
+            FilterHelpers.Register(new SomeFilter());
+            FilterHelpers.UnRegister(new SomeFilter());
+            Parser<Data> stringSolver = new Parser<Data>().WithTag(new DataNameTag());
+            var validationResult = stringSolver.Validate("The {DataName:Some} is in the kitchen.");
+            Assert.That(validationResult.IsValid, Is.False);
+        }
+    }
+}
