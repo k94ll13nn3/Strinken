d168c9921b0f7424a7a859c6ef9f1fc469649225
Keuvain <k94ll13nn3@outlook.fr>
2017-06-17 12:01:31 +0200

    Add new parsing methods

--- src/Strinken/Engine/ParsingMethod.cs
+++ src/Strinken/Engine/ParsingMethod.cs
@@ -13,6 +13,26 @@
         /// <summary>
         /// Parse all the string.
         /// </summary>
-        Full
+        Full,
+
+        /// <summary>
+        /// Parse a binary number (0 or 1).
+        /// </summary>
+        Binary,
+
+        /// <summary>
+        /// Parse an octal number (0 to 7).
+        /// </summary>
+        Octal,
+
+        /// <summary>
+        /// Parse a decimal number (0 to 9).
+        /// </summary>
+        Decimal,
+
+        /// <summary>
+        /// Parse an hexadecimal number [0-9a-fA-F].
+        /// </summary>
+        Hexadecimal,
     }
 }
