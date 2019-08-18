af67e19cd45277948e46a91c2771811f1a0576b0
Keuvain <k94ll13nn3@outlook.fr>
2019-08-18 15:41:30 +0200

    Fix typo in coverage script

--- coverage.ps1
+++ coverage.ps1
@@ -2,6 +2,6 @@ dotnet tool install -g dotnet-reportgenerator-globaltool
 dotnet tool install -g coverlet.console
 dotnet build
 coverlet .\tests\Strinken.Public.Tests\bin\Debug\netcoreapp2.2\Strinken.Public.Tests.dll --target "dotnet" --targetargs "test .\tests\Strinken.Public.Tests\Strinken.Public.Tests.csproj --no-build" --format cobertura --output coverage.public.xml
-coverlet .\tests\Strinken.Tests\bin\Debug\netcoreapp2.2Strinken.Tests.dll --target "dotnet" --targetargs "test .\tests\Strinken.Tests\Strinken.Tests.csproj --no-build" --format cobertura --output coverage.xml
+coverlet .\tests\Strinken.Tests\bin\Debug\netcoreapp2.2\Strinken.Tests.dll --target "dotnet" --targetargs "test .\tests\Strinken.Tests\Strinken.Tests.csproj --no-build" --format cobertura --output coverage.xml
 reportgenerator -reports:"coverage.xml;coverage.public.xml" -targetdir:coverage -reporttypes:"HtmlInline_AzurePipelines"
 ii coverage\index.htm
