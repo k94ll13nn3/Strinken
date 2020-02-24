4ffd2691a381dde2a1c09cb82cec326d335cd139
Kévin Gallienne <k94ll13nn3@outlook.fr>
2020-02-24 20:08:11 +0100

    Update tools & tests to .NET Core 3.1

--- build/Strinken.Build/Program.cs
+++ build/Strinken.Build/Program.cs
@@ -40,8 +40,8 @@ namespace Strinken.Build
                     Run("dotnet", "tool update -g dotnet-reportgenerator-globaltool");
                     Run("dotnet", "tool update -g coverlet.console");
                     Run("dotnet", "build");
-                    Run("coverlet", @".\tests\Strinken.Public.Tests\bin\Debug\netcoreapp2.2\Strinken.Public.Tests.dll --target ""dotnet"" --targetargs ""test .\tests\Strinken.Public.Tests\Strinken.Public.Tests.csproj --no-build"" --format cobertura --output coverage.public.xml");
-                    Run("coverlet", @".\tests\Strinken.Tests\bin\Debug\netcoreapp2.2\Strinken.Tests.dll --target ""dotnet"" --targetargs ""test .\tests\Strinken.Tests\Strinken.Tests.csproj --no-build"" --format cobertura --output coverage.xml");
+                    Run("coverlet", @".\tests\Strinken.Public.Tests\bin\Debug\netcoreapp3.1\Strinken.Public.Tests.dll --target ""dotnet"" --targetargs ""test .\tests\Strinken.Public.Tests\Strinken.Public.Tests.csproj --no-build"" --format cobertura --output coverage.public.xml");
+                    Run("coverlet", @".\tests\Strinken.Tests\bin\Debug\netcoreapp3.1\Strinken.Tests.dll --target ""dotnet"" --targetargs ""test .\tests\Strinken.Tests\Strinken.Tests.csproj --no-build"" --format cobertura --output coverage.xml");
                     Run("reportgenerator", @"-reports:""coverage.xml;coverage.public.xml"" -targetdir:coverage -reporttypes:""HtmlInline_AzurePipelines""");
                     Run("powershell", "ii coverage/index.htm");
                 });
